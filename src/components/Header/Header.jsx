87b46ca3bd042d28b84b362057472210e5ec469d
VilosmZX <jravaell@gmail.com>
2022-07-25 16:07:50 +0700

    ~

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import styles from './Header.module.css';
+
+const Header = () => {
+  return (
+    <div className={styles.header}>
+        <div>
+            <span>H</span>
+            <span>E</span>
+            <span>L</span>
+            <span>L</span>
+            <span>O</span>
+        </div>
+    </div>
+  )
+}
+
+export default Header
